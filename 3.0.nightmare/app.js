71e88ef6d9f3a5db75da3f97113c7ae78492b8c0
Mauricio Reis <mreis@simbiox.com.br>
2016-10-14 11:02:24 -0300

    Up

--- 3.0.nightmare/app.js
+++ 3.0.nightmare/app.js
@@ -4,12 +4,9 @@ var Nightmare  = require('nightmare')
   , vo         = require('vo')
   , fs         = require('fs')
   , request    = require('request')
-  , account    = require('../account.js')
-  , username   = account.instagram.username
-  , password   = account.instagram.password
-  , userToLike = process.argv[2] || "marciegottschalk"
-  , Slack      = require('../2.1.cheerio/slack.js')
-  ;
+  , account    = require('./account.js')
+  , username   = account.username
+  , password   = account.password;
 
 vo(run)(function(err, result) {
   if (err) throw err;
@@ -23,78 +20,32 @@ function *run() {
 
   console.log("Esperando tela de login");
   yield nightmare
-    .useragent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36')
-    .goto('https://www.instagram.com/accounts/login/')
-    .wait()
-    .wait('form button');
+    .goto('https://runrun.it/pt-BR')
+    .wait('.login a')
+    .click('.login a')
+    .wait('[name="user_session[email]"]');
 
   console.log(`Preenchendo dados de ${username}`);
   yield nightmare
     .evaluate(function(username, password){
-      var event = new Event('input', { bubbles: true });
-      document.querySelector('[name=username]').value = username;
-      document.querySelector('[name=username]').dispatchEvent(event);
-      document.querySelector('[name=password]').value = password;
-      document.querySelector('[name=password]').dispatchEvent(event);
+      document.querySelector('[name="user_session[email]"]').value = username;
+      document.querySelector('[name="user_session[password]"]').value = password;
     }, username, password)
     .wait(600)
-    .click('form button')
+    .click('.button-login')
     .wait(600);
 
-  console.log(`Navegando até ${userToLike}`);
+
+
+  console.log('Iniciando a primeira tarefa')
   yield nightmare
-    .goto(`https://instagram.com/${userToLike}/`)
-    .wait();
-
-  console.log(`Abrindo a primeira foto encontrada`);
-  yield nightmare
-    .click(`[href*="?taken-by=${userToLike}"]`) // clica na primeira foto
-    .wait().wait(1000)
-    .screenshot('./images/antes.png')
-    
-  console.log(`Verificando se a foto ja foi curtida`);
-  let isLiked = yield nightmare
-    .evaluate(function(){
-      return document.querySelectorAll('.coreSpriteHeartFull').length;
-    });
-  console.log(`Foto curtida? ${isLiked}`);
-
-  if(!isLiked){ // Se a foto nao foi curtida ainda
-    console.log(`Curtindo a foto por você :)`);
-    yield nightmare
-      .evaluate(function(){
-        document.querySelector('.coreSpriteHeartOpen').click();
-      })
-      .wait(300)
-      .screenshot('./images/depois.png')
-
-    console.log(`Pegando a URL da imagem`);
-    var src = yield nightmare
-      .evaluate(function(){
-        return document.querySelector('[role="dialog"] article > div img').src
-      });
-
-    meAvisa(src);
-  }
+    .wait('.enterprise-logo-img')
+    .wait('.task-title.word-breaker')
+    .click('.task-title.word-breaker .task-action')
+    .wait('.task-play')
+    .click('.task-play')
+    .wait(500)
 
   console.log("done in " + (Date.now()-x) + "ms");
   yield nightmare.end();
-}
-
-function meAvisa(url){
-  var slack = new Slack(account.slack.token);
-  var msg   = `${url}
-  Te liga magrão, tu curtiu essa foto! Comenta com ela depois.
-  `;
-  
-  slack.sendMessage(account.slack.user, msg);
-}
-
-function downloadImage(src){
-  console.log('Iniciando download da imagem');
-  request({ url: src, encoding: 'binary'}, function(err, res, body){
-    fs.writeFile('./images/foto.png', body, 'binary', function(err){
-      console.log(`Imagem salva em foto.png`);
-    });
-  });
 }
